92b8a41d196aaf9b57925a93cd61df9a61316427
rahulsahukar <rahul.sahukar@gmail.com>
2016-08-20 19:21:27 +0530

    Added overloaded function

--- ValidArgs.java
+++ ValidArgs.java
@@ -17,23 +17,39 @@ public class ValidArg extends EvalFunc<Long> {
 		int y = (int) input.get(1);
 		return (long) Math.pow(x, y);
 	}
-	
-	public Schema outputSchema(Schema input) {
-		
 		if(input.size()!=2 ){
 			throw new RuntimeException("Expecting Two Arguments!");
 		}
 		
 		try{
-		if(input.getField(0).type != DataType.INTEGER || input.getField(1).type != DataType.INTEGER){
-			throw new RuntimeException("Either of the fields is not of correct datatype. Expecting only integers!");
+			if ((input.getField(0).type != DataType.DOUBLE && input.getField(0).type != DataType.INTEGER)
+					|| input.getField(1).type != DataType.INTEGER){
+			throw new RuntimeException("Either of the fields is not of correct datatype! >>> " + input.getField(0).type +" " + input.getField(1).type );
 		}
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
 		return new Schema(new Schema.FieldSchema(null, DataType.LONG));
+	}
+	
+	public List<FuncSpec> getArgToFuncMapping(){
+		
+		List<FuncSpec> list = new ArrayList<FuncSpec>();
+		
+		Schema s = new Schema();
+		s.add(new Schema.FieldSchema(null, DataType.INTEGER));
+		s.add(new Schema.FieldSchema(null, DataType.INTEGER));
+		list.add(new FuncSpec(this.getClass().getName(), s));
+		
+		s = new Schema();
+		s.add(new Schema.FieldSchema(null, DataType.DOUBLE));
+		s.add(new Schema.FieldSchema(null, DataType.INTEGER));
+		list.add(new FuncSpec(this.getClass().getName(), s));
+		
+		return list;
 		
 	}
+	
 }
 
